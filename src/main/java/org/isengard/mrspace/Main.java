c9ddd322f01914200abf0d1052b1824046d1c618
Merijn Hooghwerff <mhooghwerff@student.scalda.nl>
2024-03-02 12:59:48 +0100

    Added login function.

--- src/main/java/org/isengard/mrspace/Main.java
+++ src/main/java/org/isengard/mrspace/Main.java
@@ -1,17 +1,38 @@
 package org.isengard.mrspace;
 
+import discord4j.core.DiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.User;
+import discord4j.core.GatewayDiscordClient;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        DiscordClient client;
+        GatewayDiscordClient gateway;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Please insert the bot token:");
+
+        while (true) {
+            try {
+                client = DiscordClient.create(input.next());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Please insert a valid token!");
+            }
         }
+
+        gateway = client.login().block();
+        assert gateway != null;
+
+        gateway.on(ReadyEvent.class).subscribe(tmod -> {
+            User self = tmod.getSelf();
+            System.out.println("Logged in as " + self.getUsername());
+        });
+
     }
 }
