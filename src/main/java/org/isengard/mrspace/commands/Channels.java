6a71eb0b5d9632d28472230dcdc2f2aa0d8d2dfe
Merijn Hooghwerff <mhooghwerff@student.scalda.nl>
2024-04-15 12:42:01 +0200

    Revert "code cleanup"
    This reverts commit f58b29741daac4c7e4429b34b1bb6c2e735c1404.

--- src/main/java/org/isengard/mrspace/commands/Channels.java
+++ src/main/java/org/isengard/mrspace/commands/Channels.java
@@ -1,7 +1,9 @@
 package org.isengard.mrspace.commands;
 
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.entity.channel.Channel;
 
+import java.util.ArrayList;
 
 public class Channels extends Command{
     @Override
